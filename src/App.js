d5a28163b478e007f156a50b5cead6263f7e65ab
keithawess <61606760+keithwess@users.noreply.github.com>
2021-07-02 10:53:40 -0500

    Adds Routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Redirect} from "react-router-dom";
-import {setState} from "react";
+import {BrowserRouter as Router, Route, Redirect, Switch, NavLink} from "react-router-dom";
+import {useState} from "react";
+import Login from './components/Login';
+import Search from './components/Search';
+import Favorites from './components/Favorites';
 
 function App() {
 
-  const [loggedInUser, setLoggedInUser] = setState(null);
+  const [loggedInUser, setLoggedInUser] = useState(null);
 
 
   return (
     <div className="App">
       Wallpapers...
 
+    {!loggedInUser && <Login setLoggedInUser={setLoggedInUser}/>}
 
+    {loggedInUser &&
+      <Router>
+        <nav>
+          <NavLink activeClassName="active" exact to="/">Search</NavLink>
+          <NavLink activeClassName="active" to="/favorites">Favorites</NavLink>
+        </nav>
+        <Switch>
+          <Route exact path="/"><Search /></Route>
+          <Route path="/favorites"><Favorites /></Route>
+          <Route path="*"><Redirect to="/"/></Route>
+        </Switch>
+      </Router>
+    }
 
     </div>
   );
